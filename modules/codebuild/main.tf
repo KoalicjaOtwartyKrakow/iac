722ee319dc65ecb4fa2d32fcd20cbc9c891d39b6
Michal Mar <maak@gft.com>
2022-03-08 13:12:38 +0100

    Add cloudbuild backend logbucket

--- modules/codebuild/main.tf
+++ modules/codebuild/main.tf
@@ -1,14 +1,16 @@
 locals {
   frontend_bucket_name = "${var.project}-codebuild-fronted-logs"
-}
-
-moved {
-  from = google_storage_bucket.log-bucket
-  to   = module.fronted_log_bucket.google_storage_bucket.log-bucket
+  backend_bucket_name  = "${var.project}-codebuild-backend-logs"
 }
 
 module "fronted_log_bucket" {
   source   = "./logbucket"
   name     = local.frontend_bucket_name
+  location = var.location
+}
+
+module "backend_log_bucket" {
+  source   = "./logbucket"
+  name     = local.backend_bucket_name
   location = var.location
 }
