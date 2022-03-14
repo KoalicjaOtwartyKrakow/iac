8587cbe199ce24844bf3a14477e00d15cc50a2bb
Michał Łazowik <mlazowik@gmail.com>
2022-03-14 21:26:28 +0100

    [build] Deploy functions in parallel (#38)
    * [build] Deploy functions in parallel
    
    * Fix syntax + format
    
    * Make the comment on paralellism clearer

--- modules/codebuild/backend/main.tf
+++ modules/codebuild/backend/main.tf
@@ -54,6 +54,8 @@ resource "google_cloudbuild_trigger" "build-trigger" {
           "--runtime=python39",
           "--set-env-vars=PROJECT_ID=613656411888"
         ]
+
+        wait_for = ["-"] # The "-" applied to all steps means that they will run in parallel.
       }
     }
 
