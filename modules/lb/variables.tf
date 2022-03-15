1bc1ef76039a8d46c60efff1c85c496c926d18cb
Michał Łazowik <mlazowik@gmail.com>
2022-03-15 14:54:04 +0100

    Allow requests from localhost:3000 to dev backends (#42)

--- modules/lb/variables.tf
+++ modules/lb/variables.tf
@@ -4,6 +4,17 @@ variable "gcp_project" {
   nullable    = false
 }
 
+variable "env_type" {
+  type        = string
+  description = "Dev or prod. Dev is less secure, for example backend CORS allows requests from localhost:3000"
+  nullable    = false
+
+  validation {
+    condition     = contains(["dev", "prod"], var.env_type)
+    error_message = "Allowed env types: dev, prod."
+  }
+}
+
 variable "frontend_bucket_name" {
   type        = string
   description = "Frontend bucket name"
